95508b9eaab791a8d23c76080993b1c28ede685a
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-31 01:54:20 +0600

    array add

--- variable-array-condition.js
+++ variable-array-condition.js
@@ -1,1 +1,16 @@
-var mangoPrice = 34;
+var bottolColor = 'yellow';
+var bottolQuantity = 3;
+var waterFull = false;
+
+// array
+var iteams = ['banana', 'pencile', 'mug', 'pen'];
+var iteamIndex = iteams.indexOf('pen');
+console.log(iteamIndex);
+
+
+iteams.push('mobile');
+iteams.push('leptop');
+// console.log(iteams);
+iteams.pop();
+console.log(iteams);
+
