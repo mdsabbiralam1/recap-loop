ac57d1daa6d69152a8514ada11c4ed1ae144893e
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-31 03:06:08 +0600

    added recap

--- while-loop.js
+++ while-loop.js
@@ -1,7 +1,18 @@
 var roastGiven = 0;
 
 while (roastGiven < 7) {
-               console.log('Roast den please..!!');
+               // console.log('Roast den please..!!');
                roastGiven++;
-               console.log(roastGiven)
+               // console.log(roastGiven)
 }    
+
+
+
+// recap
+var classRoll = 0;
+
+while (classRoll < 10) {
+               console.log('Present sir');
+               classRoll++;
+               console.log(classRoll);
+}
