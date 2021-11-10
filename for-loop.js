ef57e3cfb4b2726577731afd1d57ea8afd4e0e66
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-11 02:14:09 +0600

    added while loop

---
+++ for-loop.js
@@ -0,0 +1,10 @@
+// while loop
+// var roastGiven = 0;
+// while (roastGiven < 7) {
+//                console.log('Roast den , please');
+//                roastGiven++;
+// }
+// for loop
+for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+               console.log('roast den, please...')
+}
