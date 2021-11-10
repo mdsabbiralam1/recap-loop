ae6c58f084d806c830b06da2cd7f3b4b893ef21b
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-11 01:13:34 +0600

    added recap while lop

--- while-loop-example.js
+++ while-loop-example.js
@@ -1,12 +1,19 @@
+/*
 var number = 0;
 
 while (number <= 10) {
                // console.log(number)
                number++;
 }
-\
 // odd Number 
 while (number <= 7) {
                console.log(number);
                number = number + 2;
+} */
+
+// recap while loop
+var num = 1; // loop variable
+while (num <= 10) {
+               console.log(num);
+               num++;
 }
