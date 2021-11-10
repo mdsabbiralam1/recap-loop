ff3ee64614f0382e3ad6101825d62a25cf308a15
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-11 03:18:07 +0600

    added  for loop

--- for-loop.js
+++ for-loop.js
@@ -4,7 +4,21 @@
 //                console.log('Roast den , please');
 //                roastGiven++;
 // }
+
+/*
 // for loop
+console.log('cheak a for loop')
 for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
                console.log('roast den, please...')
+               console.log(roastGiven);
+}
+*/
+
+// racap for loop
+// for (j = 0; j < 7; j++) {
+//                console.log(i);
+// }
+
+for (i = 1; i <= 10; i++) {
+               console.log(i); 
 }
