9777f8126c11b3deb7d99278c45cd90c309d3ae6
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-11 01:03:33 +0600

    added some code

--- while-loop-example.js
+++ while-loop-example.js
@@ -1,6 +1,12 @@
 var number = 0;
 
 while (number <= 10) {
-               console.log(number)
+               // console.log(number)
                number++;
+}
+\
+// odd Number 
+while (number <= 7) {
+               console.log(number);
+               number = number + 2;
 }
