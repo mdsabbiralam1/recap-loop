b44fce80fc9e8ac1c24e4d66235f1e0b7fa78f61
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-14 16:36:21 +0600

    added recap for loop

---
+++ array-loop.js
@@ -0,0 +1,14 @@
+/*
+var roll = [34, 43, 64, 32, 75, 34];
+for(var i = 0; i < roll.length; i++){
+               var element = roll[i]
+               console.log(element);
+}
+*/
+
+// recap
+var iteams = ['bottle', 'camera', 'mobile', 'honda', 'airpod'];
+for(var i = 0; i < iteams.length; i++) {
+               var iteamsElement = iteams[i]
+               console.log(iteamsElement);
+}
